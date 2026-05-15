0d409bc85eca1ae781e3175307b5c7bf405e0621
GouthamReddy7-dev <gouthamreddy7272@gmail.com>
2026-05-15 17:27:56 +0530

    Log DATABASE_URL and query parameters in get_leads
    Added print statements to log DATABASE_URL and query parameters.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_engine():
     try:
 
         DATABASE_URL = os.getenv("DATABASE_URL")
+        print(DATABASE_URL)
 
         engine = create_engine(
             DATABASE_URL
@@ -59,6 +60,7 @@ def get_leads(
     try:
 
         engine = get_engine()
+        print(DATABASE_URL)
 
         if engine is None:
             return {"error": "Database connection failed"}
